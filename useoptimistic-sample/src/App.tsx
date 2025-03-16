00bf53fa49aa58bddb07a0b980fc6f92caca6b5e
sneko <supernekocat31@gmail.com>
2025-03-16 13:29:39 +0900

    feat:add useoptimistic-sample

---
+++ useoptimistic-sample/src/App.tsx
@@ -0,0 +1,23 @@
+import { Thread } from './components/Thread'
+import { useState } from "react"
+import { deliverMessage } from "./action"
+import './App.css'
+
+function App() {
+  const [messages, setMessages] = useState([
+    { text: "こんにちは！", sending: false, key: 1 }
+  ])
+
+  async function sendMessage(formData) {
+    const sentMessage = await deliverMessage(formData.get("message"))
+    setMessages((messages) => [...messages, { text: sentMessage }])
+  }
+
+  return (
+    <div>
+      <Thread messages={messages} sendMessage={sendMessage} />
+    </div>
+  )
+}
+
+export default App
---
+++ useoptimistic-sample/src/components/Thread.tsx
@@ -0,0 +1,37 @@
+import { useOptimistic, useRef } from "react"
+
+export function Thread({ messages, sendMessage }) {
+  const formRef = useRef()
+
+  const [optimisticMessages, addOptimisticMessage] = useOptimistic(
+    messages,
+    (state, newMessage) => [
+      ...state,
+      {
+        text: newMessage,
+        sending: true
+      }
+    ]
+  )
+
+  async function formAction(formData) {
+    addOptimisticMessage(formData.get("message"))
+    formRef.current.reset()
+    await sendMessage(formData)
+  }
+
+  return (
+    <>
+      <form action={formAction} ref={formRef}>
+        <input type="text" name="message" placeholder="テキストを入力" />
+        <button type="submit">送信</button>
+      </form>
+      {optimisticMessages.map((message, index) => (
+        <div key={index}>
+          {message.text}
+          {!!message.sending && <small> (送信中...)</small>}
+        </div>
+      ))}
+    </>
+  )
+}
